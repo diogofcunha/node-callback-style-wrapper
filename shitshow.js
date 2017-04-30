024dbe97711dec9d4ff76aa0202f7b5db0dce49d
diogofcunha <diogofncunha@gmail.com>
2017-04-30 17:39:25 +0100

    First commit

--- shitshow.js
+++
@@ -1,6 +0,0 @@
-const wrapper = require('./index.js');
-const fs = require('fs');
-
-wrapper(fs.readdir, './node_modules')
-   .then((x) => { console.log("Cool", x) })
-   .catch(() => { console.error("shit") });
